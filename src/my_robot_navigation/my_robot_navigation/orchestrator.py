611995491f9d95235cb25e3caaedb104b7c96178
alvaro-mediina <alvaro.mediina2003@gmail.com>
2026-07-23 11:52:28 -0300

    feat: :sparkles: Consulto las transformaciones para la posiciones actuales del robot

--- src/my_robot_navigation/my_robot_navigation/orchestrator.py
+++ src/my_robot_navigation/my_robot_navigation/orchestrator.py
@@ -4,7 +4,8 @@ import rclpy
 
 from geometry_msgs.msg import PoseStamped
 from rclpy.node import Node
-
+from tf2_ros import Buffer, TransformListener
+from rclpy.time import Time
 
 class Orchestrator(Node):
 
@@ -12,6 +13,9 @@ class Orchestrator(Node):
         super().__init__("orchestrator")
 
         self.current_goal = None
+        self.tf_buffer = Buffer() # Guardo las transformaciones
+        self.tf_listener = TransformListener(self.tf_buffer, self) #Escucho /tf y /tf_static
+
 
         self.goal_subscription = self.create_subscription(
             PoseStamped,       
@@ -50,6 +54,42 @@ class Orchestrator(Node):
             f" \torientación: x={qx:.3f}, y={qy:.3f}, z={qz:.3f}, w={qw:.3f}"
         )
 
+        transform = self.get_robot_pose()
+
+        if transform is None:
+            self.get_logger().warning(
+            "No se pudo consultar la posición actual del robot."
+            )
+            return
+
+        robot_x = transform.transform.translation.x
+        robot_y = transform.transform.translation.y
+        robot_z = transform.transform.translation.z
+
+        orientation = transform.transform.rotation
+
+        # Chequear la posición actual del robot
+        # self.get_logger().info(
+        #     "Posición actual del robot:\n"
+        #     f"  x={robot_x:.2f}\n"
+        #     f"  y={robot_y:.2f}\n"
+        #     f"  z={robot_z:.2f}\n"
+        #     f"  orientación quaternion: "
+        #     f"x={orientation.x:.3f}, "
+        #     f"y={orientation.y:.3f}, "
+        #     f"z={orientation.z:.3f}, "
+        #     f"w={orientation.w:.3f}"
+        # )
+
+
+
+    def get_robot_pose(self):
+        try:
+            transform = self.tf_buffer.lookup_transform("map", "base_link", Time())
+        except Exception as error:
+            self.get_logger().warning(f"No se pudo obtener map -> baselink {error}")
+            return None
+        return transform
 
 def main(args=None):
     rclpy.init(args=args)
